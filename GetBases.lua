c09b11c5ea19b312d9b858ac4cdb400d31c22f5e
Zanikes <rupertcm88@proton.me>
2023-10-29 16:16:29 -0400

    Updated base list

--- GetBases.lua
+++ GetBases.lua
@@ -4,22 +4,19 @@ local function GetFile(FileName)
 end
 
 local BaseList = {
+	['America Town Base'] = GetFile('AmericaTown'),
+	['America Base Revamp'] = GetFile('AmericaTownRevamp'),
 	['Bunker Base'] = GetFile('BunkerBase'),
-	['America Base'] = GetFile('AmericaTown'),
-	['Tower'] = GetFile('Tower'),
-	['Weapons'] = GetFile('Weapons'),
-	['Kin Tower'] = GetFile('KinTower'),
-	['Dave\'s House'] = GetFile('DavesHouse'),
 	['Cage'] = GetFile('Cage'),
+	['Dave\'s House'] = GetFile('DavesHouse'),
+	['Kin Tower'] = GetFile('KinTower'),
 	['Large Cage'] = GetFile('LargeCage'),
-	['Forever Part'] = GetFile('ForeverPart'),
-	['Radio Tower'] = GetFile('RadioTower'),
 	['MA Firestation'] = GetFile('MAFireStation'),
 	['MA Extra'] = GetFile('MAExtra'),
-	['Giant Cock & Balls'] = GetFile('pp'),
-	['Amogus'] = GetFile('sussywussy'),
-	['America Base v.2'] = GetFile('AmericaTown2'),
-	['TA Base'] = GetFile('TABase')
+	['Radio Tower'] = GetFile('RadioTower'),
+	['TA Base'] = GetFile('TABase'),
+	['Tower'] = GetFile('Tower'),
+	['Weapons'] = GetFile('Weapons')
 }
 
 return BaseList
